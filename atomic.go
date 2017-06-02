7905cb4ea73c2c37615d087beda0ca37351bf4e7
Ryougi <ryougi.nevermore@hotmail.com>
2017-06-02 18:44:03 +0800

    add doc.

--- atomic.go
+++ atomic.go
@@ -9,41 +9,45 @@ import (
 
 const atomic_num_add_retry_times = 64
 
+// AtomicNumber new one with default
 func NewAtomicNumber() *AtomicNumber {
 	n := new(AtomicNumber)
-	n.val = uint64(0)
+	n.number = uint64(0)
 	n.retry = atomic_num_add_retry_times
 	n.mu = new(sync.Mutex)
 	return n
 }
 
+// AtomicNumber number(uint64) increase by cas and mutex when over retry times
 type AtomicNumber struct {
-	val uint64
+	number uint64
 	retry int
 	mu *sync.Mutex
 }
 
-func (n *AtomicNumber) Add() (uint64, error) {
+// increase number by cas and mutex when over retry times
+func (n *AtomicNumber) Increase() (uint64, error) {
 	retry := atomic_num_add_retry_times
-	if n.val == math.MaxUint64 {
-		return n.val, errors.New("number is max.")
+	if n.number == math.MaxUint64 {
+		return n.number, errors.New("number is max.")
 	}
-	for !atomic.CompareAndSwapUint64(&n.val, n.val, n.val + uint64(1)) {
+	for !atomic.CompareAndSwapUint64(&n.number, n.number, n.number + uint64(1)) {
 		retry --
 		if retry > 0 {
 			continue
 		}
 		n.mu.Lock()
-		n.val = n.val + uint64(1)
+		n.number = n.number + uint64(1)
 		n.mu.Unlock()
 		break
 	}
-	return n.val, nil
+	return n.number, nil
 }
 
+// set 0 into number
 func (n *AtomicNumber) Reset() {
 	n.mu.Lock()
-	n.val = uint64(0)
+	n.number = uint64(0)
 	n.mu.Unlock()
 }
 
